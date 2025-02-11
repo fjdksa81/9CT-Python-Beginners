4aa9bc0a1cc56419f067b979add703d556e83815
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-02-11 11:09:26 +1100

    Update code_challenge.py
    End of lesson work.

--- python-for-beginners/13 - Functions/code_challenge.py
+++ python-for-beginners/13 - Functions/code_challenge.py
@@ -5,10 +5,17 @@
 # operation: the word 'add' or 'subtract'
 # the function should return the result of the two numbers added or subtracted
 # based on the value passed in for the operator
-#
+def calculator(first_number, second_number, operation):
+    if operation.upper() == 'add':
+        return(float(first_number)) + (float(second_number))
+    elif operation.upper() == 'subtract':
+        return(float(first_number)) - (float(second_number))
+    else:
+        return ("Invalid operation")
+
 # Test your function with the values 6,4, add 
 # Should return 10
-#
+calculator(6,4,'add')
 # Test your function with the values 6,4, subtract 
 # Should return 2
 # 
